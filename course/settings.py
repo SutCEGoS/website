70da371fc452ebc72beb2be6f3e66c0bcf99bc08
mmmdamin <mmmdamin@gmail.com>
2014-12-31 15:56:37 +0330

    init migration for app 'base'

--- course/settings.py
+++ course/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -36,6 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'south',
+
+    'base',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,7 +79,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
