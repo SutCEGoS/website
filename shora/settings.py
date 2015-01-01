3cbbc01fc091d1ee52f44ab9a9b64cf20c31e3e7
mmmdamin <mmmdamin@gmail.com>
2015-01-01 13:28:54 +0330

    fix database setting again :))

--- shora/settings.py
+++ shora/settings.py
@@ -69,9 +69,8 @@ DATABASES = {
     }
 }
 
-if os.environ.has_key('DATABASE_URL'):
-    import dj_database_url
-
+import dj_database_url
+if dj_database_url.config():
     DATABASES['default'] = dj_database_url.config()
 
 LANGUAGE_CODE = 'en-us'
