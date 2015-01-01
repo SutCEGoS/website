f83d0cf77b6a261487990f181dd5e5aaad0aad5d
mmmdamin <mmmdamin@gmail.com>
2015-01-01 19:18:30 +0330

    search & courses_list

--- objection/urls.py
+++ objection/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('objection.views',
                        url(r'^requests/?$', 'requests', name='requests'),
-                       url(r'api/v1/search', 'search', name='objection_api_search')
+                       url(r'ajax/search', 'search', name='objection_api_search'),
+                       url(r'ajax/courses', 'get_courses', name='get_courses_list'),
 
 )
