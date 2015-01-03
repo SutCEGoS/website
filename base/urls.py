1c22ff9bd215262fa0a7c5baf76c6144b2125b2a
mmmdamin <mmmdamin@gmail.com>
2015-01-03 23:36:31 +0330

    change password

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('base.views',
                        # url(r'^$', 'home', name='home'),
                        url(r'^/?$', 'home', name='home'),
                        url(r'^login/?$', 'login', name='login'),
                        url(r'^create-account-from-file-by-admin-qsc/$', 'create_accounts', name='create-account'),
+                       url(r'^change-password/?$', 'password_reset_change', name='password_reset_change'),
 
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
