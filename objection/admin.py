1e89275c294f9b3ad7a19d3428e214a60b3f135d
mmmdamin <mmmdamin@gmail.com>
2015-01-04 19:54:43 +0330

    change admin functions

--- objection/admin.py
+++ objection/admin.py
@@ -5,7 +5,7 @@ from objection.models import Objection
 
 
 class ObjectionAdmin(admin.ModelAdmin):
-    actions = ['mark_as_read', 'mark_as_unread', 'mark_as_confirmed', 'mark_as_unconfirmed']
+    actions = ['mark_as_unconfirmed', 'mark_as_waiting']
     list_display = ['id', 'message', 'level', 'field', 'category', 'read', 'confirmed']
     fieldsets = (
         (u'پیام', {
@@ -17,23 +17,13 @@ class ObjectionAdmin(admin.ModelAdmin):
         }),
     )
 
-    def mark_as_read(modeladmin, request, queryset):
+    def mark_as_waiting(modeladmin, request, queryset):
         queryset.update(read=True)
 
-    mark_as_read.short_description = u"علامت زدن موارد انتخابی به عنوان خوانده شده"
-
-    def mark_as_unread(modeladmin, request, queryset):
-        queryset.update(read=False)
-
-    mark_as_unread.short_description = u"علامت زدن موارد انتخابی به عنوان خوانده نشده"
-
-    def mark_as_confirmed(modeladmin, request, queryset):
-        queryset.update(confirmed=True)
-
-    mark_as_confirmed.short_description = u"علامت زدن موارد انتخابی به عنوان تایید شده"
+    mark_as_waiting.short_description = u"علامت زدن موارد انتخابی به عنوان منتظر پاسخ"
 
     def mark_as_unconfirmed(modeladmin, request, queryset):
-        queryset.update(confirmed=False)
+        queryset.update(read=False)
 
     mark_as_unconfirmed.short_description = u"علامت زدن موارد انتخابی به عنوان تایید نشده"
 
