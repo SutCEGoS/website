60f1c495d20aa235f96719d520b3868426844def
Seyed Mohammad Razi Moosavi <sm.razi.m@gmail.com>
2015-01-04 20:56:53 +0330

    remove_search_filter javascript

---
+++ static/js/remove_search_filter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by seyed on 1/4/15.
+ */
+&(document).ready(function() {
+    $("#remove_search_filter").onclick(function(){
+        $('form')[0].reset();
+    });
+});
--- static/js/searchform.js
+++ static/js/searchform.js
@@ -91,6 +91,14 @@ $window.on('load', function() {
             $('.second-course-div').fadeOut();
             $('.course-name-div').fadeOut();
         }
+        var $remove_search_filter = $("#remove_search_filter");
+        if (value > 0) {
+            $remove_search_filter.show();
+            $remove_search_filter.removeClass("hide");
+        }
+        else {
+            $remove_search_filter.hide();
+        }
     }
 
 
