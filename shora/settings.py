0fdf703800299e01c50a19ee556082f1b7418aa7
mmmdamin <mmmdamin@gmail.com>
2015-01-04 23:43:17 +0330

    change email password

--- shora/settings.py
+++ shora/settings.py
@@ -156,4 +156,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shora.cesharif@gmail.com'
-EMAIL_HOST_PASSWORD = 'butterflyeffect'
+EMAIL_HOST_PASSWORD = 'butterflyeffect2'
