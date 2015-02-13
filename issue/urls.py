ad9c10fd895f82b8ee81b051a68c539264f140ab
MJafar Mashhadi <raindigital2007@gmail.com>
2015-02-13 13:29:49 +0330

    HTML e issue OK shod

--- issue/urls.py
+++ issue/urls.py
@@ -6,5 +6,4 @@ urlpatterns = patterns('issue.views',
                        url(r'^ajax/items$', 'search_issue', name='get_objections_list_issue'),
                        url(r'^ajax/add$', 'add_issue', name='add_objection_issue'),
                        url(r'^ajax/me_too$', 'add_me_too', name='add_me_too_issue'),
-
 )
