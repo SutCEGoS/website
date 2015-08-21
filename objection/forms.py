f318e58b491c4874a8b07d619c7882fc0fed8152
MohammadAmin Sabbaghian <sabbaghian@arsh.co>
2015-08-21 11:09:19 +0430

    برطرف کردن باگ فاجعه :))))

--- objection/forms.py
+++ objection/forms.py
@@ -7,7 +7,7 @@ from course.views import get_current_year
 
 __author__ = 'mjafar'
 
-from objection.models import Objection, Reply
+from objection.models import Objection
 
 
 class MessageForm(forms.ModelForm):
@@ -25,10 +25,10 @@ class MessageForm(forms.ModelForm):
             else:
                 css_class = 'full-width'
             self.fields[field].widget.attrs.update({'class': css_class})
-        self.fields['offered_course'].queryset = Objection.objects.filter(term=settings.CURRENT_TERM,
-                                                                          year=get_current_year())
-        self.fields['second_course'].queryset = Objection.objects.filter(term=settings.CURRENT_TERM,
-                                                                         year=get_current_year())
+        self.fields['offered_course'].queryset = OfferedCourse.objects.filter(term=settings.CURRENT_TERM,
+                                                                              year=get_current_year())
+        self.fields['second_course'].queryset = OfferedCourse.objects.filter(term=settings.CURRENT_TERM,
+                                                                             year=get_current_year())
 
     def clean(self):
         cd = super(MessageForm, self).clean()
