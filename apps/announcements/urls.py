4d10b9aefb97033a1b1d86d2fdec71392cd363c5
MJafar Mashhadi <raindigital2007@gmail.com>
2016-08-06 12:34:43 +0430

    updated urls

--- apps/announcements/urls.py
+++ apps/announcements/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^/?$", views.all_announcements, name='all-announcements'),
-    url(r"^(?P<announcement_id>[0-9]+)/show/$", views.show_announcement, name='show-announcement')
+    url(r"^(?P<announcement_id>[0-9]+)/show/$", views.show_announcement, name='show-announcement'),
+    url(r"^$", views.all_announcements, name='all-announcements'),
 ]
--- apps/base/urls.py
+++ apps/base/urls.py
@@ -3,11 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', 'home', name='home'),
-    url(r'^/?$', views.index, name='home'),
     url(r'^members/?$', views.members, name='members'),
     url(r'^login/?$', views.login, name='login'),
     url(r'^logout/?$', views.logout, name='logout'),
     url(r'^create-account-from-file-by-admin-qsc/$', views.create_accounts, name='create-account'),
     url(r'^change-password/?$', views.password_reset_change, name='password_reset_change'),
+    url(r'^$', views.index, name='home'),
 ]
