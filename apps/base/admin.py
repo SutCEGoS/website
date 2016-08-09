662ba82bd7c9f9736a2d5e3148c196876b3b38ab
MJafar Mashhadi <raindigital2007@gmail.com>
2016-08-09 23:15:47 +0430

    removed buggy field from admin

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -5,6 +5,6 @@ from .models import Member
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('get_full_name', 'username', 'email', 'join_date', 'is_active', 'std_id')
+    list_display = ('get_full_name', 'username', 'email', 'is_active', 'std_id')
     search_fields = ('first_name', 'last_name', 'username', 'std_id')
     list_filter = ('is_active', 'start_year')
