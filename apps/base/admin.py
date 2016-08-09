375ce63fce3c245124021b188e28f5a36b6d5cdc
MJafar Mashhadi <raindigital2007@gmail.com>
2016-08-09 23:17:16 +0430

    more links in admin!

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -8,3 +8,4 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = ('get_full_name', 'username', 'email', 'is_active', 'std_id')
     search_fields = ('first_name', 'last_name', 'username', 'std_id')
     list_filter = ('is_active', 'start_year')
+    list_display_links = ('get_full_name', 'username', 'email')
