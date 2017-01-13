84234b92e6dd8000a64d76b0277cc8f9567b9dec
MJafar Mashhadi <raindigital2007@gmail.com>
2017-01-13 20:24:37 +0330

    Removed update courses url

--- apps/course/urls.py
+++ apps/course/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^update-courses-list-from-term-inatot-ppqwrtopf/$', views.update_courses_list, name='update-courses-list')
+    # url(r'^update-courses-list-from-term-inatot-ppqwrtopf/$', views.update_courses_list, name='update-courses-list')
 ]
