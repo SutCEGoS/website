8601aa5d59dcafe3d34b768d70b591a61643e799
MJafar Mashhadi <raindigital2007@gmail.com>
2017-01-16 22:00:12 +0330

    حذف urlای که deprecate شد

--- apps/course/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    # url(r'^update-courses-list-from-term-inatot-ppqwrtopf/$', views.update_courses_list, name='update-courses-list')
-]
