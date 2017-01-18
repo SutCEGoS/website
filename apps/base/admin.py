b629cea992bde624bd84017ea169685aa601ddc6
MJafar Mashhadi <raindigital2007@gmail.com>
2017-01-18 13:11:16 +0330

    Hijack admin

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from hijack_admin.admin import HijackUserAdminMixin
 
 from .models import Member
 
 
 @admin.register(Member)
-class MemberAdmin(admin.ModelAdmin):
+class MemberAdmin(admin.ModelAdmin, HijackUserAdminMixin):
     list_display = ('get_full_name', 'username', 'email', 'is_active', 'std_id', 'hijack_field')
     search_fields = ('first_name', 'last_name', 'username', 'std_id', 'email')
     list_filter = ('is_active', 'start_year')
