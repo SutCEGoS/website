b020394b66738ec54cabab2d37fdb13a8d0a4a5d
MJafar Mashhadi <raindigital2007@gmail.com>
2017-01-20 16:12:07 +0330

    Removed add users view

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,18 +1,12 @@
 from django.contrib.auth import login as dj_login
 from django.contrib.auth import logout as dj_logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.hashers import make_password
-from django.contrib.sites.requests import RequestSite
-from django.core.exceptions import PermissionDenied
-from django.core.mail import EmailMessage
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.template import loader
 
 from apps.announcements.models import Announcement
 from .forms import *
-from .models import Member
 
 
 def home(request):
@@ -65,77 +59,6 @@ def login(request):
     return render(request, 'login.html', {
         'form': form,
         'next': next,
-    })
-
-
-def save_file(in_memory_file):
-    import os
-    from django.core.files.storage import default_storage
-    from django.core.files.base import ContentFile
-    from django.conf import settings
-
-    path = default_storage.save('data_file/' + str(in_memory_file), ContentFile(in_memory_file.read()))
-    tmp_file = os.path.join(settings.MEDIA_ROOT, path)
-    return tmp_file
-
-
-@login_required
-def create_accounts(request):
-    if not request.user.is_superuser:
-        raise PermissionDenied
-    message = ""
-    if request.method == "POST":
-        emails_file = request.FILES.get('emails')
-        if not emails_file:
-            message = "file is required"
-        else:
-            emails_file = save_file(emails_file)
-            file_lines = open(emails_file, 'r').readlines()
-            for i in range(len(file_lines)):
-                line = file_lines[i]
-                splitted_line = line.split('-')
-                try:
-                    new_email = splitted_line[0].lower()
-                except:
-                    message += str(i)
-                try:
-                    new_std_id = splitted_line[1]
-                except:
-                    new_std_id = ""
-                new_password = Member.objects.make_random_password(length=10)
-                if '@' in new_email:
-                    new_username = new_email[:new_email.find('@')]
-                else:
-                    new_username = new_email
-                    new_email = new_username + "@ce.sharif.edu"
-                try:
-                    member = Member.objects.get(username=new_username)
-                except:
-                    try:
-                        context = {
-                            'site': RequestSite(request),
-                            'username': new_username,
-                            'password': new_password,
-                            'secure': request.is_secure(),
-                        }
-                        subject = loader.render_to_string("account_create/new_account_email_subject.txt", context).strip()
-                        text_body = loader.render_to_string("account_create/new_account_email.txt", context).strip()
-
-                        msg = EmailMessage(subject=subject, from_email="shora.cesharif@gmail.com", to=[new_email], body=text_body)
-                        msg.send()
-                        new_member = Member.objects.create(username=new_username,
-                                                           std_id=new_std_id,
-                                                           email=new_email,
-                                                           password=make_password(new_password))
-                    except:
-                        message += "%d\n" % i
-
-            if message:
-                message += "making account for this lines is not possible, please contact A\'min"
-            else:
-                message = "success"
-    return render(request, 'create_account.html', {
-        'message': message,
     })
 
 
