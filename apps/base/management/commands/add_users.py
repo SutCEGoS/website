6d524d03a228769225da9f1d92264cb9883bb48f
MJafar Mashhadi <raindigital2007@gmail.com>
2017-01-20 16:04:44 +0330

    Set year

--- apps/base/management/commands/add_users.py
+++ apps/base/management/commands/add_users.py
@@ -4,7 +4,7 @@ from django.core.management import BaseCommand
 from django.db import transaction
 from django.template import loader
 
-from apps.base.models import Member
+from apps.base.models import Member, EducationalYear
 
 
 class Command(BaseCommand):
@@ -22,6 +22,7 @@ class Command(BaseCommand):
 
         file = open(str(emails_file), 'r')
         l_number = 0
+        edu_years = dict()
         try:
             for line in file.read().split('\n'):
                 l_number += 1
@@ -32,9 +33,16 @@ class Command(BaseCommand):
                 try:
                     new_std_id = splitted_line[0]
                     new_email = splitted_line[1].lower()
+                    if new_std_id[:2] in edu_years:
+                        new_start_year = edu_years[new_std_id[:2]]
+                    else:
+                        ey, created = EducationalYear.objects.get_or_create(year=1300 + int(new_std_id[:2]))
+                        new_start_year = ey
+                        edu_years[new_std_id[:2]] = ey
                 except:
                     self.stdout.write(self.style.ERROR('line ' + str(l_number) + ' has some errors. [line=' + line + ']'))
                     had_error = True
+                    continue
 
                 new_password = Member.objects.make_random_password(length=7)
                 if '@' in new_email:
@@ -65,7 +73,8 @@ class Command(BaseCommand):
                             member = Member.objects.create(username=new_username,
                                                            std_id=new_std_id,
                                                            email=new_email,
-                                                           password=make_password(new_password))
+                                                           password=make_password(new_password),
+                                                           start_year=new_start_year)
                             self.stdout.write(self.style.SUCCESS('Created account for ') + new_username + ' (' + new_std_id + ')')
                     except:
                         self.stdout.write(self.style.ERROR('Could not create account for ' + str(l_number) + 'th line. [line=' + line + ']'))
