60835cb8dd123a12f7baf24dbff5be4999a06b0b
Mjg79 <mahdi.javan.gh2@gmail.com>
2018-10-05 19:44:33 +0330

    payment finished

--- apps/locker/admin.py
+++ apps/locker/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import rack,sell
 # Register your models here...
 
+class rackAdmin(admin.ModelAdmin):
+    list_display = ['name','receiver','payment', 'receivie_date']
+
 class sellAdmin(admin.ModelAdmin):
     list_display = ['user' , 'locker' , 'is_success']
 
-admin.site.register(rack)
+admin.site.register(rack , rackAdmin)
 admin.site.register(sell , sellAdmin)
