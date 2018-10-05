9474700966e6e48c58099f688bc80fff8f6056c4
Kiarash Golzadeh <kiarash.gl@gmail.com>
2018-10-05 19:32:15 +0330

    Scroll to ادامه after choosing locker

--- static/js/locker.js
+++ static/js/locker.js
@@ -1,16 +1,25 @@
 var lockerLetter = 'M';
 
 function changeLetter(ch) {
-    $("#locker-title").html("کمد "+ch);
+    $("#locker-title").html("کمد " + ch);
     lockerLetter = ch;
     $(".locker > .row > .col ").each(function () {
-        var my  = $(this).html();
-        my = my.slice(my.length-2,my.length);
-        $(this).html(lockerLetter+my);
+        var my = $(this).html();
+        my = my.slice(my.length - 2, my.length);
+        $(this).html(lockerLetter + my);
     });
 }
+
 $(".locker > .row > .col ").click(function () {
         $("#chosen-locker").val($(this).html());
         $("#lockerModal").modal('toggle');
+        $("#lockerModal").on("hidden.bs.modal", function () {
+            continueButton.scrollIntoView(
+                {
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+        });
+
     }
 );
