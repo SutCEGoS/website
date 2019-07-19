70b24415c69e48aa6d06a123f53a9a8ee2fd148d
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-07-19 19:37:44 +0430

    امکان فیلتر کردن superuser ها در پنل ادمین

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -6,8 +6,8 @@ from .models import Member
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin, HijackUserAdminMixin):
-    list_display = ('get_full_name', 'username', 'email', 'is_active', 'std_id', 'hijack_field')
+    list_display = ('get_full_name', 'username', 'email', 'is_active', 'is_superuser', 'std_id', 'hijack_field')
     search_fields = ('first_name', 'last_name', 'username', 'std_id', 'email')
-    list_filter = ('is_active', 'start_year')
+    list_filter = ('is_active', 'start_year', 'is_superuser', 'is_staff')
     list_display_links = ('get_full_name', 'username', 'email')
     readonly_fields = ('hijack_field',)
