148b6a65627c020627e088095967478ddaa1c63d
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-16 11:45:01 +0430

    add charge button

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     url(r'^logout/?$', views.logout, name='logout'),
     url(r'^change-password/?$', views.password_reset_change, name='password_reset_change'),
     url(r'^$', views.index, name='home'),
-    url(r'^profile',views.profile,name='profile'),
-    url(r'^complete_profile',views.complete_profile,name='complete_profile'),
-    url(r'^under_construction/',views.under_construction,name='under_construction')
+    url(r'^profile', views.profile, name='profile'),
+    url(r'^complete_profile', views.complete_profile, name='complete_profile'),
+    url(r'^under_construction/', views.under_construction, name='under_construction')
     # url('^password_reset/', auth_views.password_reset, {'template_name': 'password_reset/password_reset_form.html'}, name='password_reset'),
     # url('^', include('django.contrib.auth.urls')),
 ]
